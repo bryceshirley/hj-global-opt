ec4c1f09d047e81fd6dd783597ce526be160084a
bryceshirley <bryce.shirley@stfc.ac.uk>
2024-10-21 17:54:43 +0100

    All 3D functions tested

--- test_functions1D.py
+++ test_functions1D.py
@@ -1,25 +1,27 @@
-import torch
 import numpy as np
 
-# 1D functions
-# Sine function
-def Sin(x):
-    return torch.sin(x)
-
-def Sinc(x):
-    return torch.where(x == 0, torch.tensor(1.0), torch.sin(x) / x)
-
-# Example of a 1D function with multiple local minima and one global minimum
-def MultiMinimaFunc(x):
-    return 5*torch.sin(x) + 0.1*x**2
-
-# ----------------- Numpy Versions -----------------
 def Sin_numpy(x):
+    """Compute the sine of x using numpy."""
+    # Global minima at x=3*pi/2 + 2*k*pi where k is an integer, gamma = None
     return np.sin(x)
 
 def Sinc_numpy(x):
-    return np.sinc(x / np.pi)  # np.sinc is normalized as sin(pi*x)/(pi*x)
+    """Compute the sinc function: sin(x)/(x) using numpy."""
+    # Global minima at x=4.49341 and -4.49341, gamma = 2.75909 (restrict x0>0 for positive min)
+    return np.sinc(x/np.pi)
 
 def MultiMinimaFunc_numpy(x):
-    return 5*np.sin(x) + 0.1*x**2
+    """Compute a multi-minima function: 5*sin(x) + 0.1*x^2."""
+    # Global minima at x=-1.51035, gamma = 1.89777
+    return 5 * np.sin(x) + 0.1 * x**2
+
+def MultiMinimaAbsFunc_numpy(x):
+    """Compute a non-differentiable multi-minima function: 5*|sin(x)| + 0.1*x^2."""
+    # Global minima at (1,0.1), gamma = 1.43457
+    return 5 * np.abs(np.sin(0.5*x-1)) + 0.1 * x**2
+
+def DiscontinuousFunc_numpy(x):
+    """Compute a discontinuous function with a global minimum."""
+    # Global minima at x=2, gamma = 7
+    return np.where(x < 2, 5 * np.sin(x) + 0.1 * x**2,  0.5*(x - 2)**2-20)#np.where(x < 2, np.abs(x**2/(x+1) + 80)+x**2,  0.5*(x - 2)**2+2)
 
